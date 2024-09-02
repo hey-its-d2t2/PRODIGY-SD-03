aa816581c8c308c8e98fd1735958ae5bee3cfb8e
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-02 18:45:23 +0530

    Contact Repository updated.

--- src/main/java/com/PRODIGY_SD_03/Repository/ContactRepository.java
+++ src/main/java/com/PRODIGY_SD_03/Repository/ContactRepository.java
@@ -2,6 +2,8 @@ package com.PRODIGY_SD_03.Repository;
 
 import com.PRODIGY_SD_03.Entity.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContactRepository extends JpaRepository<Contact, Long> {
 }
