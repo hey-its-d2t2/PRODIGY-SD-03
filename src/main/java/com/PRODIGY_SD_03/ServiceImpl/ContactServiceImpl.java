8740a6fdd3a29b8ec56582877e3d67cbe837db6e
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-02 17:22:12 +0530

    Updated methods as FindByFirstName and added two method FindByMiddleName, FindByLastName.

--- src/main/java/com/PRODIGY_SD_03/ServiceImpl/ContactServiceImpl.java
+++ src/main/java/com/PRODIGY_SD_03/ServiceImpl/ContactServiceImpl.java
@@ -41,20 +41,22 @@ public class ContactServiceImpl implements ContactService {
        if(optContact.isPresent()){
            Contact contact = optContact.get();
 
-           contact.setName(contactDetails.getName());
+           contact.setFirstName(contactDetails.getFirstName());
+           contact.setMiddleName(contactDetails.getMiddleName());
+           contact.setLastName(contactDetails.getLastName());
            contact.setPrimaryPhoneNumber(contactDetails.getPrimaryPhoneNumber());
            contact.setSecondaryPhoneNumber(contactDetails.getSecondaryPhoneNumber());
            contact.setPrimaryEmailAddress(contactDetails.getPrimaryEmailAddress());
            contact.setSecondaryEmailAddress(contactDetails.getSecondaryEmailAddress());
            contact.setAddress(contactDetails.getAddress());
-           contact.setCompany(contact.getCompany());
+           contact.setCompany(contactDetails.getCompany());
            contact.setJobTitle(contactDetails.getJobTitle());
            contact.setNotes(contactDetails.getNotes());
            contact.setPhotoUrl(contactDetails.getPhotoUrl());
            contact.setWebsite(contactDetails.getWebsite());
            contact.setBirthday(contactDetails.getBirthday());
            contact.setNickname(contactDetails.getNickname());
-           contact.setRelationship(contact.getRelationship());
+           contact.setRelationship(contactDetails.getRelationship());
            contact.setLabel(contactDetails.getLabel());
            contact.setDepartment(contactDetails.getDepartment());
            contact.setOfficeLocation(contactDetails.getOfficeLocation());
@@ -66,4 +68,136 @@ public class ContactServiceImpl implements ContactService {
     }
 
 
+// ---------
+    @Override
+    public void deleteContact(Long id) {
+
+    }
+
+    @Override
+    public List<Contact> findContactByFirstName(String name) {
+        return List.of();
+    }
+
+    @Override
+    public List<Contact> findContactByMiddleName(String name) {
+        return List.of();
+    }
+
+    @Override
+    public List<Contact> findContactByLastName(String name) {
+        return List.of();
+    }
+
+    @Override
+    public List<Contact> findContactByNickName(String nickname) {
+        return List.of();
+    }
+
+    @Override
+    public List<Contact> findContactByBirthday(String birthday) {
+        return List.of();
+    }
+
+    @Override
+    public List<Contact> findContactByEmail(String email) {
+        return List.of();
+    }
+
+    @Override
+    public List<Contact> findContactBySecEmail(String secEmail) {
+        return List.of();
+    }
+
+    @Override
+    public List<Contact> findContactByPrimaryPhone(String priNumber) {
+        return List.of();
+    }
+
+    @Override
+    public List<Contact> findContactBySecondaryPhone(String secNumber) {
+        return List.of();
+    }
+
+    @Override
+    public List<Contact> findContactsByCompany(String company) {
+        return List.of();
+    }
+
+    @Override
+    public List<Contact> findContactsByJobTitle(String jobTitle) {
+        return List.of();
+    }
+
+    @Override
+    public List<Contact> findContactsByLabel(String label) {
+        return List.of();
+    }
+
+    @Override
+    public List<Contact> findContactsByRelationship(String relationship) {
+        return List.of();
+    }
+
+    @Override
+    public List<Contact> findContactsByDepartment(String department) {
+        return List.of();
+    }
+
+    @Override
+    public List<Contact> findContactsByOfficeLocation(String officeLocation) {
+        return List.of();
+    }
+
+    @Override
+    public List<Contact> findContactsByLanguage(String language) {
+        return List.of();
+    }
+
+    //----
+   /* @Override
+    public void deleteContact(Long id) {
+        contactRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Contact> searchContactsByName(String name) {
+        return contactRepository.findByNameContainingIgnoreCase(name);
+    }
+
+    @Override
+    public List<Contact> findContactsByCompany(String company) {
+        return contactRepository.findByCompanyContainingIgnoreCase(company);
+    }
+
+    @Override
+    public List<Contact> findContactsByJobTitle(String jobTitle) {
+        return contactRepository.findByJobTitleContainingIgnoreCase(jobTitle);
+    }
+
+    @Override
+    public List<Contact> findContactsByLabel(String label) {
+        return contactRepository.findByLabelContainingIgnoreCase(label);
+    }
+
+    @Override
+    public List<Contact> findContactsByRelationship(String relationship) {
+        return contactRepository.findByRelationshipContainingIgnoreCase(relationship);
+    }
+
+    @Override
+    public List<Contact> findContactsByDepartment(String department) {
+        return contactRepository.findByDepartmentContainingIgnoreCase(department);
+    }
+
+    @Override
+    public List<Contact> findContactsByOfficeLocation(String officeLocation) {
+        return contactRepository.findByOfficeLocationContainingIgnoreCase(officeLocation);
+    }
+
+    @Override
+    public List<Contact> findContactsByLanguage(String language) {
+        return contactRepository.findByLanguageContainingIgnoreCase(language);
+    }
+*/
 }
