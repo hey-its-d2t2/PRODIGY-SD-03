9eea471b6ef85afcbe184f236ae29694f9dfa4be
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-02 18:44:34 +0530

    Find By Name service class implemented.

--- src/main/java/com/PRODIGY_SD_03/ServiceImpl/FindByNameImpl.java
+++ src/main/java/com/PRODIGY_SD_03/ServiceImpl/FindByNameImpl.java
@@ -1,28 +1,35 @@
 package com.PRODIGY_SD_03.ServiceImpl;
 
 import com.PRODIGY_SD_03.Entity.Contact;
+import com.PRODIGY_SD_03.Repository.NameRepository;
 import com.PRODIGY_SD_03.Service.FindByName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class FindByNameImpl implements FindByName {
+    @Autowired
+    private NameRepository  nameRepository;
+
     @Override
-    public List<Contact> findContactByFirstName(String name) {
-        return List.of();
+    public List<Contact> findContactByFirstName(String firstName) {
+        return nameRepository.findByFirstName(firstName);
     }
 
     @Override
-    public List<Contact> findContactByMiddleName(String name) {
-        return List.of();
+    public List<Contact> findContactByMiddleName(String middleName) {
+        return nameRepository.findByMiddleName(middleName);
     }
 
     @Override
-    public List<Contact> findContactByLastName(String name) {
-        return List.of();
+    public List<Contact> findContactByLastName(String lastName) {
+        return nameRepository.findByLastName(lastName);
     }
 
-    @Override
+   @Override
     public List<Contact> findContactByNickName(String nickname) {
-        return List.of();
+        return nameRepository.findBynickname(nickname);
     }
 }
