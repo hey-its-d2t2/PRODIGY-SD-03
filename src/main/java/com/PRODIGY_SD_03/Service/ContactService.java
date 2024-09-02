2813c57bad600435593ce7dd6ab3cee6e9758de2
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-02 18:15:03 +0530

    Contact Service updated and seperated other service methods to related new service classes, here only CRUD Operations like addContact, getContactById, getAllContact, updateContact, deleteContact.

--- src/main/java/com/PRODIGY_SD_03/Service/ContactService.java
+++ src/main/java/com/PRODIGY_SD_03/Service/ContactService.java
@@ -17,38 +17,5 @@ public interface ContactService {
 
     void deleteContact(Long id);
 
-    //find by Name
-    List<Contact> findContactByFirstName(String name);
-    List<Contact> findContactByMiddleName(String name);
-    List<Contact> findContactByLastName(String name);
-    List<Contact> findContactByNickName(String nickname);
-
-    //Find by Birth Date
-    List<Contact> findContactByBirthday(String birthday);
-
-    //Find by Email
-    List<Contact> findContactByEmail(String email);
-    List<Contact> findContactBySecEmail(String secEmail);
-
-    //Find by Phone Number
-    List<Contact> findContactByPrimaryPhone(String priNumber);
-    List<Contact> findContactBySecondaryPhone(String secNumber);
-
-    //Find By address
-    List<Contact> findContactByAddress(String address);
-
-    List<Contact> findContactsByCompany(String company);
-
-    List<Contact> findContactsByJobTitle(String jobTitle);
-
-    List<Contact> findContactsByLabel(String label);
-
-    List<Contact> findContactsByRelationship(String relationship);
-
-    List<Contact> findContactsByDepartment(String department);
-
-    List<Contact> findContactsByOfficeLocation(String officeLocation);
-
-    List<Contact> findContactsByLanguage(String language);
 
 }
