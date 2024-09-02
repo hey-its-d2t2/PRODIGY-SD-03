d698ce84b9cf8821258eeafc13e9845b49d721ba
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-02 14:05:34 +0530

    Initial commit: Project created with some dependencies like Spring Web, Lombok, Spring Data JPa, Thymeleaf,Spring Boot Dev Tools and another configuration which is by default comes with Maven project from start.spring.io.

---
+++ src/main/java/com/PRODIGY_SD_03/ProdigySd03InternshipApplication.java
@@ -0,0 +1,13 @@
+package com.PRODIGY_SD_03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdigySd03InternshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdigySd03InternshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PRODIGY_SD_03/ProdigySd01InternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PRODIGY_SD_03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdigySd01InternshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
