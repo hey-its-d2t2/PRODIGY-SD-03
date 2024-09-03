31aee2ca27f50a47703cd138302109d4a1422cbf
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-03 09:21:10 +0530

    User Repository added.

---
+++ src/main/java/com/PRODIGY_SD_03/Repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.PRODIGY_SD_03.Repository;
+
+import com.PRODIGY_SD_03.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findByEmail(String email);
+}
