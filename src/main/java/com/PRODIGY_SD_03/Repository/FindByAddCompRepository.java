4c8e306185346d27edde60a188d50798ef591b6e
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-03 07:15:10 +0530

    Find by Address, Company, JobTitle, Label, Department, OfficeLocation, Language methods decleared/defined in Repository.

---
+++ src/main/java/com/PRODIGY_SD_03/Repository/FindByAddCompRepository.java
@@ -0,0 +1,25 @@
+package com.PRODIGY_SD_03.Repository;
+
+import com.PRODIGY_SD_03.Entity.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public interface FindByAddCompRepository extends JpaRepository<Contact, Long> {
+
+    List<Contact> findContactByAddress(String address);
+
+    List<Contact> findContactsByCompany(String company);
+
+    List<Contact> findContactsByJobTitle(String jobTitle);
+
+    List<Contact> findContactsByLabel(String label);
+
+    List<Contact> findContactsByDepartment(String department);
+
+    List<Contact> findContactsByOfficeLocation(String officeLocation);
+
+    List<Contact> findContactsByLanguage(String language);
+}
