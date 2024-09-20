dee9561be32764ffedf4ac6c4445914b1c1683b2
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-20 15:18:30 +0530

    Entity updated

--- src/main/java/com/PRODIGY_SD_03/Entity/Contact.java
+++ src/main/java/com/PRODIGY_SD_03/Entity/Contact.java
@@ -5,7 +5,7 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.*;
 import lombok.*;
 
-import java.util.Date;
+import java.sql.Date;
 
 
 @Getter
@@ -20,7 +20,7 @@ public class Contact {
     private Long id;
 
     @NotBlank(message = "Name is required")
-    @Size(max = 100, min = 3, message = "Name must be between 3 and 100 characters.")
+    @Size(max = 30, min = 3, message = "Name must be between 3 and 100 characters.")
     private String name;
 
     @NotBlank(message = "Phone number is required")
@@ -30,24 +30,24 @@ public class Contact {
 
     @NotBlank(message = "Email address is required")
     @Email(message = "Email should be valid")
-    @Size(max = 100, message = "Email cannot exceed 100 characters")
+    @Size(max = 50, message = "Email cannot exceed 100 characters")
     private String email;
 
-    @Size(max = 255, message = "Address cannot exceed 255 characters")
+    @Size(max = 50, message = "Address cannot exceed 255 characters")
     private String address;
 
     private String photoUrl; // Optional photo field
 
-   /* @Temporal(TemporalType.DATE)
-    private Date birthday; // Optional birthday field*/
+    @Temporal(TemporalType.DATE)
+    private Date birthday; // Optional birthday field
 
-   private String birthday; // Optional birthday field
+   //private String birthday; // Optional birthday field
 
     @Column(name = "nickname")
-    @Size(max = 255, message = "Nickname cannot exceed 255 characters")
+    @Size(max = 25, message = "Nickname cannot exceed 255 characters")
     private String nickname; // Optional nickname
 
-    @Size(max = 50, message = "Relationship cannot exceed 50 characters")
+    @Size(max = 25, message = "Relationship cannot exceed 50 characters")
     private String relationship; // Optional relationship field (e.g., friend, family)
 
 
